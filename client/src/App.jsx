343cd021ff223791418de1783cb9d55f64d50545
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-04-02 14:24:01 +0530

    Update App.jsx bfgghghbgfhb

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import Navbar from './components/Navbar';
 import ShopScreen from './screens/ShopScreen';
@@ -20,22 +20,22 @@ const App = () => {
   return (
     <div>
       <Navbar/>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/shop" element={<ShopScreen/>} />
-          <Route path="/aboutus" element={<AboutUsScreen/>} />
-          <Route path="/contactus" element={<ContactUs />} />
-          <Route path="/blogs" element={<BlogsScreen />} />
-          <Route path="/login" element={<LoginScreen />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/cart" element={<CartScreen />} />
-          <Route path="/shop/item_details" element={<Show_item_details />} />
-          <Route path="/blogchild" element={<BlogChild />} />
-          <Route path="/myprofile" element={<MyFormComponent />} />
-          <Route path='/orderOptions' element={<OrderProductOptions/>}/>
-        </Routes>
-      </Router>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/shop" element={<ShopScreen />} />
+        <Route path="/aboutus" element={<AboutUsScreen />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/blogs" element={<BlogsScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/cart" element={<CartScreen />} />
+        <Route path="/shop/item_details" element={<Show_item_details />} />
+        <Route path="/blogchild" element={<BlogChild />} />
+        <Route path="/myprofile" element={<MyFormComponent />} />
+        <Route path="/orderOptions" element={<OrderProductOptions />} />
+      </Routes>
+    </BrowserRouter>
       <Footer/>
     </div>
   )
