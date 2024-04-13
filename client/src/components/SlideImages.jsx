2e1226983cca9f5254ef52554182ac1199a94c65
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-04-14 03:31:12 +0530

    gbngn

---
+++ client/src/components/SlideImages.jsx
@@ -0,0 +1,20 @@
+import poster1 from '../assets/poster1.png'
+import slide1 from '../assets/slide1.png';
+import slide2 from '../assets/slide2.png'
+import slide3 from '../assets/slide3.png'
+export const slideImages =[
+    {
+        images:poster1
+    },
+    {
+        images:slide1
+    }
+    ,
+    {
+        images:slide2
+    }
+    ,
+    {
+        images:slide3
+    }
+]
---
+++ client/src/components/Slideshow.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+// import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import poster1 from "../assets/poster1.png";
+import './Slideshow.css'
+import { slideImages } from "./SlideImages";
+function Slideshow() {
+  const [current, setCurrent] = useState(0);
+  const length = slideImages.length;
+  // const videoRef=useRef()
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+    // videoRef.current.play();
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+    // videoRef.current.play();
+  };
+
+  useEffect(() => {
+    // Function to transition to the next slide every 10 seconds
+    const intervalId = setInterval(() => {
+      nextSlide();
+    }, 10000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [current, length]);
+
+  if (!Array.isArray(slideImages) || slideImages.length <= 0) {
+    return null;
+  }
+  const defaultOptions = (dboyid) => {
+    return {
+      loop: true,
+      autoplay: true,
+      animationData: dboyid,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice",
+      },
+    };
+  };
+  console.log(slideImages)
+  return (
+    <>
+      <section className="slider">
+        <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+        <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
+        {slideImages.map((slide, index) => (
+          <div
+            className={index === current ? "slide active" : "slide"}
+            key={index}
+          >
+            {index === current && (
+              <img src={slide.images} alt={index} className="Imageslide" />
+            )}
+          </div>
+        ))}
+      </section>
+    </>
+  );
+}
+
+export default Slideshow;
