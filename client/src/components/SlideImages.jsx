ab5188a56bad23a53b14f6c0875da19ebf691778
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-04-26 19:34:45 +0530

    Update SlideImages.jsx xfgfb

--- client/src/components/SlideImages.jsx
+++ client/src/components/SlideImages.jsx
@@ -1,11 +1,8 @@
-import poster1 from '../assets/poster1.png'
+// import poster1 from '../assets/poster1.png'
 import slide1 from '../assets/slide1.png';
 import slide2 from '../assets/slide2.png'
 import slide3 from '../assets/slide3.png'
 export const slideImages =[
-    {
-        images:poster1
-    },
     {
         images:slide1
     }
