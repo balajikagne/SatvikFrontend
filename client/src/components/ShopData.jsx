f51a1e538b8697b16131c1d8834e11e5e40f3321
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-04-27 11:19:40 +0530

    Update ShopData.jsx sddsfdb

--- client/src/components/ShopData.jsx
+++ client/src/components/ShopData.jsx
@@ -40,10 +40,7 @@ function ShopData({item}) {
                />
                <div className="price_varient"><h4 style={{textAlign:'center',fontSize:"1.2rem",marginBottom:"-8px"}}>{item.name}</h4>
                <h4 style={{textAlign:'center',marginBottom:"4px"}}>{item.varient[0]['frist']}</h4>
-               <div className="price_box" onClick={()=>{More_info(item)}} style={{marginLeft:"12px"}}>
-{/*                 <p style={{color:'grey', textDecoration: "line-through" }}>{item.oldPrice}</p> */}
-                <p style={{textAlign:'center'}}>₹ {item.field[0]['frist']}</p>
-               </div>
+               <p style={{textAlign:'center'}}>Rs {item.field[0]['frist']}</p>
                </div>
                
                <div className="star_Shop_Container">{start}</div>
